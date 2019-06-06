f3faaec027ca4d2bfedccb2a087e25f79250ee02
MacheterArt <yahor.vas@gmail.com>
2019-06-06 17:21:37 +0200

    Last

---
+++ Brick Brecker Game/Assets/BrickScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    public int points;
+    public int hitsToBreak;
+    public int brickHits;
+    public Sprite hitSprite;
+    public Sprite hitSprite_2;
+
+    public void BreakBrick()
+    {
+        hitsToBreak--;
+        GetComponent<SpriteRenderer>().sprite = hitSprite;
+    }
+
+    public void BreakBrick_2()
+    {
+        hitsToBreak--;
+        GetComponent<SpriteRenderer>().sprite = hitSprite_2;
+    }
+}
---
+++ Brick Brecker Game/Assets/Prefabs/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int lives;
+    public int score;
+    public Text livesTest;
+    public Text scoreText;
+    public bool gameOver;
+    public GameObject gameOverPanel;
+    public int numberOfBricks;
+
+
+    // Start is called before the first frame update
+    void Start()      {
+        
+
+        scoreText.text = "Score: " + score;
+        livesTest.text = "Lives: " + lives;
+        numberOfBricks = GameObject.FindGameObjectsWithTag("brick").Length;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+        public void UpdateLives(int changeInLives) {
+            lives += changeInLives;
+
+        if (lives <= 0)
+        {
+            lives = 0;
+            GameOver();
+        }
+
+        livesTest.text = "Lives: " + lives;
+    }
+
+    public void UpdateScore (int points)
+    {
+        score += points;
+
+        scoreText.text = "score: " + score;
+    }
+
+    public void UpdateNumberOfBricks()
+    {
+        numberOfBricks--;
+        if(numberOfBricks <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()  {
+        gameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+    } 
+
